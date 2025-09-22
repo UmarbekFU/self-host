c9a3061f59bc157cf3b0b52eb6aaa161281aabf8
Umarbek Fazliddinovich <153717176+UmarbekFU@users.noreply.github.com>
2025-09-23 03:36:08 +0500

    Add linter configuration to suppress false warnings
    - Add .webhintrc to disable TypeScript warnings on generated files
    - Add .eslintrc.js for proper TypeScript linting of source files
    - Ignore .svelte-kit/ directory from linting
    - Source code has no actual TypeScript errors
    
    The warnings were from Microsoft Edge Tools analyzing auto-generated
    SvelteKit files, which should not be linted.

---
+++ app/web/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:svelte/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn'
+  },
+  ignorePatterns: [
+    '.svelte-kit/',
+    'build/',
+    'node_modules/'
+  ]
+};
